2da3229a25060c9eccb2e0afe03cdfc2157223be
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2021-12-22 10:02:08 +0800

    Create 08.crawler.py

---
+++ 08.crawler.py
@@ -0,0 +1,16 @@
+
+
+
+
+def get_token(html):
+    '''
+    from bs4 import BeautifulSoup
+    https://cuiqingcai.com/1319.html
+    筛选提取页面的html内容
+    :param html:
+    第一次访问网页存在隐藏项目authenticity_token
+    '''
+    soup = BeautifulSoup(html,'lxml')
+    res = soup.find("input",attrs={"name":"authenticity_token"})
+    token = res["value"]
+    return token
