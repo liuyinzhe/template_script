6b12ef8d160bc886c2c9430a576beae2cf39f701
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2021-12-22 10:13:52 +0800

    Update 08.crawler.py

--- 08.crawler.py
+++ 08.crawler.py
@@ -20,3 +20,23 @@ def get_token(html):
     res = soup.find("input",attrs={"name":"authenticity_token"})
     token = res["value"]
     return token
+
+
+def construction_multipart_encoded_str (new_list_tuple,boundary_str):
+    '''
+    #boundary_str
+    ------WebKitFormBoundaryhTmOInhA14nYnzK0
+    #分隔符：--
+    #单个内容结构
+    ----WebKitFormBoundaryGtgXulVY19zxw4Ub\r\nContent-Disposition: form-data; name="domain"\r\n\r\n*Microbiome\r\n
+    '''
+    new_list=[]
+    start=''
+    new_list.append(start)
+    for name,value in new_list_tuple:
+        new_str= '''\r\nContent-Disposition: form-data; name="{}"\r\n\r\n{}\r\n'''.format(name.strip(),value.strip())
+        new_list.append(new_str)
+    end_str=''
+    new_list.append(end_str)
+    final_str = boundary_str.join(new_list)+'--\r\n'
+    return final_str
