5fa1f17408980680c73eac54061f479ed715e3b4
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2021-12-22 10:08:08 +0800

    Create 09.time.py

---
+++ 09.time.py
@@ -0,0 +1,21 @@
+import sys
+import time
+import datetime
+
+
+def main():
+  pass
+
+
+if __name__ == "__main__":
+    if sys.version[0] == "3":
+        start_time = time.perf_counter()
+    else:
+        start_time = time.clock()
+    main()
+    if sys.version[0] == "3":
+        end_time = time.perf_counter()
+    else:
+        end_time = time.clock()
+    print("%s %s %s\n" % ("main()", "use", str(
+        datetime.timedelta(seconds=end_time - start_time))))
