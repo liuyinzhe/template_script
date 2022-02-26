020e5b2be1f0a019d62d5d8f8532cd0f77c98b43
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2022-02-26 20:08:11 +0800

    Create 15.docx_operate.py

---
+++ 15.docx_operate.py
@@ -0,0 +1,9 @@
+from docx import Document
+
+
+def read_docx(doc_file):
+    doc = Document(str(doc_file))
+    #输出每一段的内容
+    for para in doc.paragraphs:
+        line = str(para.text).strip()
+        print(line)
