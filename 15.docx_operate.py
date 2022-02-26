3cba810114c53cc7afa8ca2ccf08e58aa206ef01
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2022-02-26 20:08:29 +0800

    Update 15.docx_operate.py

--- 15.docx_operate.py
+++ 15.docx_operate.py
@@ -3,7 +3,7 @@ from docx import Document
 
 def read_docx(doc_file):
     doc = Document(str(doc_file))
-    #输出每一段的内容
+    #输出每一段/行的内容
     for para in doc.paragraphs:
         line = str(para.text).strip()
         print(line)
