d0b9276974c4ee77f407ef0f3b67820be8dece92
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-11-24 13:00:42 +0800

    Create 16.interval.py

---
+++ 16.interval.py
@@ -0,0 +1,32 @@
+import re
+from natsort import natsorted
+
+def determine_contain(position,regions):
+    '''
+    :param position: int
+    :param regions: List[List[int]]
+    :return: bool
+    '''
+    flag_determine = False
+    for interval in regions:
+        x,y = interval
+        if x < position < y:
+            flag_determine = True
+            break
+    return flag_determine
+    
+    
+def merge_intervals(regions):
+    """
+    :param regions: List[List[int]]
+    :return: List[List[int]]
+    """
+
+    regions.sort(key=lambda x: x[0])
+    merged = []
+    for interval in regions:
+        if not merged or merged[-1][-1] < interval[0]:
+            merged.append(interval)
+        else:
+            merged[-1][-1] = max(merged[-1][-1], interval[-1])
+    return merged
