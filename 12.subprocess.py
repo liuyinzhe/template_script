600e4757600677acc18301fdfefa8c3fd08084a9
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-08-26 16:17:00 +0800

    Update 12.subprocess.py

--- 12.subprocess.py
+++ 12.subprocess.py
@@ -11,3 +11,17 @@ def run_obs_ls_cmd(obsutil_bin, completed_obs_path):
                              stdout=subprocess.PIPE, shell=True, check=True)
     return ret.stdout
  
+
+def run_cmd(exce_bin, args_lst):
+    '''
+        param:obs_path
+        param:check_dir_list
+        #https://www.cnblogs.com/lgj8/p/12132829.html
+    '''
+    command_lst = []
+    command_lst.append(exce_bin)
+    command_lst.extend(args_lst)
+    cmd = " ".join(command_lst)
+    ret = subprocess.run(args=cmd, encoding='utf8',
+                             stdout=subprocess.PIPE, shell=True, check=True)
+    return ret.stdout
