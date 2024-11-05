0648d8cc39804a59578bb3cae19014471a7a4a10
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-11-05 15:54:04 +0800

    Update 17.interval.py

--- 17.interval.py
+++ 17.interval.py
@@ -1,6 +1,37 @@
 import re
 from natsort import natsorted
 
+def get_other_intervals(all_regions,intervals):
+    '''
+    给于0-base 区间,从all_regions中移除intervals区间,获得剩余区间
+    返回的列表区间，可用于直接截取字符串
+    '''
+    # all_regions = (0,31)
+    # intervals = [(2, 4), (7, 25)]
+    #get_other_intervals(all_regions,intervals)
+    result = [] # 不在区间内的数字
+    start_idx , end_idx = all_regions
+    for num  in range(start_idx,end_idx+1):
+        in_interval = False
+        for interval in intervals:
+            if interval[0] <= num <= interval[1]:
+                in_interval = True
+                break
+        if not in_interval:
+            result.append(num)
+    # 将结果转换为区间形式
+    result_intervals = []
+    start = None
+    for num in result:
+        if start is None:
+            start = num
+        if num + 1 not in result: # 移位+1,作为end  # 到了末尾
+            end = num
+            result_intervals.append((start, end))
+            start = None
+    return result_intervals
+
+
 def determine_contain(position,regions):
     '''
     :param position: int
